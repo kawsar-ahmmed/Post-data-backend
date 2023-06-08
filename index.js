8523600a37a0a93e499323af7ccf7e7c3eb24e4f
kawsar-ahmmed <kawsarahmmedr@gmail.com>
2023-06-09 02:48:46 +0600

    Mongo setup

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = 3001;
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 app.use(cors());
 app.use(express.json());
 
@@ -20,6 +20,36 @@ const users = [
     { id: 7, name: 'shakira', email: 'shakira@gmail.com', phone: '01299999922' },
     { id: 8, name: 'Anta', email: 'anta@gmail.com', phone: '08976677' },
 ];
+
+// Database
+
+
+const uri = "mongodb+srv://rodoy91221:Ridoy91221@cluster0.wqkufut.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+}
+});
+console.log('DB Connected')
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/users', (req, res) => {
     res.send(users);
